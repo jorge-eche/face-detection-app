142b72b2abc7d41a69bbc0136bd041203a4d76f1
Jorge Echeverria <coko.eche@gmail.com>
2023-03-26 10:28:36 +1100

    built Register component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,4 @@
-import React from "react";
-
-const SignIn = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
@@ -8,14 +6,14 @@ const SignIn = ({ onRouteChange }) => {
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                Email
+              <label className="db fw6 lh-copy f6" htmlFor="name">
+                Name
               </label>
               <input
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                type="email"
-                name="email-address"
-                id="email-address"
+                type="text"
+                name="name"
+                id="name"
               />
             </div>
             <div className="mv3">
@@ -35,17 +33,8 @@ const SignIn = ({ onRouteChange }) => {
               onClick={() => onRouteChange("home")}
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
-              value="Sign in"
+              value="Register"
             />
-          </div>
-          <div className="lh-copy mt3">
-            <p
-              onClick={() => onRouteChange("register")}
-              href="#0"
-              className="f6 link dim black db"
-            >
-              Register
-            </p>
           </div>
         </form>
       </main>
@@ -53,4 +42,4 @@ const SignIn = ({ onRouteChange }) => {
   );
 };
 
-export default SignIn;
+export default Register;
